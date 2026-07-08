194d7d151c018bac1adc4e9c570787bcd7bf3e5d
Amazing_Deodar2 <1401520875@qq.com>
2026-07-08 21:15:35 +0800

    平衡调整

--- starss/starssCode/Cards/AbyssJudgment.cs
+++ starss/starssCode/Cards/AbyssJudgment.cs
@@ -10,39 +10,53 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using starss.starssCode.Mechanics;
-
+using VoidCard = MegaCrit.Sts2.Core.Models.Cards.Void;
+using BeckonCard = MegaCrit.Sts2.Core.Models.Cards.Beckon;
 namespace starss.starssCode.Cards;
 
-public sealed class AbyssJudgment : starssCard
-{
-    public AbyssJudgment()
-        : base(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+
+    public sealed class AbyssJudgment : starssCard
     {
+        public AbyssJudgment()
+            : base(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+        {
+        }
+
+        protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [
+            new CalculationBaseVar(6M),
+            new ExtraDamageVar(4M),
+            new CalculatedDamageVar(ValueProp.Move)
+                .WithMultiplier((card, _) => CountVoidAndBeckon(card))
+        ];
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+            await DamageCmd.Attack(DynamicVars.CalculatedDamage)
+                .FromCard(this, cardPlay)
+                .Targeting(cardPlay.Target)
+                .WithHitFx("vfx/vfx_attack_slash")
+                .Execute(choiceContext);
+        }
+
+        private static decimal CountVoidAndBeckon(CardModel card)
+        {
+            return
+                PileType.Hand.GetPile(card.Owner).Cards.Count(IsVoidOrBeckon) +
+                PileType.Draw.GetPile(card.Owner).Cards.Count(IsVoidOrBeckon) +
+                PileType.Discard.GetPile(card.Owner).Cards.Count(IsVoidOrBeckon) +
+                PileType.Exhaust.GetPile(card.Owner).Cards.Count(IsVoidOrBeckon);
+        }
+
+        private static bool IsVoidOrBeckon(CardModel card)
+        {
+            return card is VoidCard || card is BeckonCard;
+        }
+
+        protected override void OnUpgrade()
+        {
+            DynamicVars.ExtraDamage.UpgradeValueBy(1M);
+        }
     }
-
-    protected override IEnumerable<DynamicVar> CanonicalVars =>
-    [
-        new CalculationBaseVar(6M),
-        new ExtraDamageVar(4M),
-        new CalculatedDamageVar(ValueProp.Move)
-            .WithMultiplier((card, _) =>
-                GlobalVoidCallCounter.GetTotalCount(card.Owner)
-            )
-    ];
-
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
-    {
-        ArgumentNullException.ThrowIfNull(cardPlay.Target);
-
-        await DamageCmd.Attack(DynamicVars.CalculatedDamage)
-            .FromCard(this,cardPlay)
-            .Targeting(cardPlay.Target)
-            .WithHitFx("vfx/vfx_attack_slash")
-            .Execute(choiceContext);
-    }
-
-    protected override void OnUpgrade()
-    {
-        DynamicVars.ExtraDamage.UpgradeValueBy(1M);
-    }
-}
